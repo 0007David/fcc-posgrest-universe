889aefb1c1404dd4718292b619255521b1720168
DaveTorrez <davidtn52@gmail.com>
2026-01-29 14:36:51 -0400

    add finished World cup DB

--- world-cup/insert_data.sh
+++ world-cup/insert_data.sh
@@ -8,3 +8,58 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE games, teams")
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT W_GOALS O_GOALS
+do 
+  
+  # si no validar si es cabecera
+  if [[ $YEAR != year ]] 
+  then    
+    # Obtenemos el team_id Winner
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    # Si no existe 
+    if [[ -z $WINNER_ID ]]
+    then
+      # insertamos el equipo
+      INSERT_TEAM_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      
+      # validamos si lo hizo con exito
+      if [[ $INSERT_TEAM_WINNER_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams, $WINNER
+      fi
+      
+      # obtenemos en nuevo team_id
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+
+    fi
+    # Obtenemos el team_id Opponent
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")    
+    # Si no existe 
+    if [[ -z $OPPONENT_ID ]]
+    then
+      # insertamos el equipo
+      INSERT_TEAM_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+
+      # validamos si lo hizo con exito
+      if [[ $INSERT_TEAM_OPPONENT_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams, $OPPONENT
+      fi
+
+      # obtenemos en nuevo team_id
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+    fi
+    # Hasta este punto ya tengo los winner_id, opponent_id
+    # insertamos en la tabla game
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $W_GOALS, $O_GOALS)")
+      
+      # validamos si lo hizo con exito
+      if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+      then
+        echo "Inserted into games, $WINNER vs $OPPONENT"
+      fi
+  fi
+done
